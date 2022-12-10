baa9e82ffd36e32bef4c4226c477527a1cf4a1ed
Daberechi Emmanuel Ngerem <emmanuelngerem05@gmail.com>
2022-12-09 20:35:09 -0800

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Empty point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int i;
+	int n;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (n = 48; n < 58; n++)
+		{
+			if (n > i)
+			{
+				for (j = 48; j < 58; j++)
+				{
+					if (j > n)
+					{
+						putchar(i);
+						putchar(n);
+						putchar(j);
+						if (i == 55 && == 56 && j == 57)
+						{
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
