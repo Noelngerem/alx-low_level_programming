faa55e2d7ff5d1f7c3a1f923c93cda3897199d5a
Daberechi Emmanuel Ngerem <emmanuelngerem05@gmail.com>
2022-12-09 19:29:15 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Empty point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10 num++)
+		printf("%d", num);
+	putchar('\n');
+	return (0);
+}
